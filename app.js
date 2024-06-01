e353a916a41748b8bc2a5d051452e6ca92bb3a00
Iftikhar Ahmad <83190084+Iftikhar4500@users.noreply.github.com>
2024-06-01 09:57:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
-let todo = [];
-let req = prompt("Please Enter the reuest");
+// let todo = [];
+// let req = prompt("Please Enter the reuest");
 
-while(true){
-  if(req == "quit"){
-    console.log("quiting app");
-    break;
-  }
-  if(req == "list"){
-    console.log("-------------");
-    for(let i=0; i<todo.length; i++){
-      console.log(i,todo[i]);
+// while(true){
+//   if(req == "quit"){
+//     console.log("quiting app");
+//     break;
+//   }
+//   if(req == "list"){
+//     console.log("-------------");
+//     for(let i=0; i<todo.length; i++){
+//       console.log(i,todo[i]);
+//     }
+//     console.log("-------------");
+//   }
+//   else if(req == "add"){
+//     let task = prompt("Please Enter the task you want to add");
+//     todo.push(task);
+//     console.log("task added");
+//   }
+//   else if (req == "delete"){
+//     let idx = prompt("Please enter the index");
+//     todo.splice(idx);
+//     console.log("task delteted");
+//   }
+//   else{
+//     console.log("Wrong request");
+//   }
+//   req = prompt("Please enter you request");
+// }
+
+
+
+
+//Using bubbling and Delegation Concept
+
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click",function(){    
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete Task";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+    ul.appendChild(item);////Adding Item inside the ul 
+    inp.value = "";
+});
+
+//using Event Delegation
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("delete");
     }
-    console.log("-------------");
-  }
-  else if(req == "add"){
-    let task = prompt("Please Enter the task you want to add");
-    todo.push(task);
-    console.log("task added");
-  }
-  else if (req == "delete"){
-    let idx = prompt("Please enter the index");
-    todo.splice(idx);
-    console.log("task delteted");
-  }
-  else{
-    console.log("Wrong request");
-  }
-  req = prompt("Please enter you request");
-}
+});
+
